50eda05fe2cf6aba7f7cc81ac71307e6efae173d
ozgurruzun <107312640+ozgurruzun@users.noreply.github.com>
2026-02-27 17:29:59 +0300

    Create config.example.h

---
+++ esp32/config.example.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define WIFI_SSID "your_wifi"
+#define WIFI_PASSWORD "your_password"
+
+#define TWILIO_ACCOUNT_SID "your_sid"
+#define TWILIO_AUTH_TOKEN "your_auth_token"
+#define TWILIO_FROM_NUMBER "+123456789"
+#define TWILIO_TO_NUMBER "+90xxxxxxxxxx"
